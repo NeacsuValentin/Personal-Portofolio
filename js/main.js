5255b3b7767355fc14049e19479dec27008f8488
ValentinNeacsu <neacsu_valentin_iulian@yahoo.ro>
2020-08-18 20:39:34 +0300

    skills section

--- js/main.js
+++ js/main.js
@@ -13,61 +13,6 @@ $('#navbar a, .btn').on('click', function(event) {
     }
 });
 
-
-// $(document).ready(function () {
-//     $('#contact-form').submit(function (e) {
-//     const form = document.querySelector('form[id="contact-form"]');
-//     const username = form.elements['firstName'].value;
-//     const userMail = form.elements['email'].value;
-//     const subject = form.elements[‘subject’].value;
-//     const message =  userMail + <br> + "Summary:"+ form.elements['message'].value;
-//     e.preventDefault();
-//     var data = {
-//     service_id: 'gmail',
-//     template_id: 'template_2SvIv66k',
-//     user_id: 'user_4zzBA7YZCHYpMTbQc2v5p',
-//     template_params: {
-//     from_name: username,
-//     to_name: 'Sucheta',
-//     subject: subject,
-//     message_html: message
-//     }
-//     };
-//     $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-//     type: 'POST',
-//     data: JSON.stringify(data),
-//     contentType: 'application/json'
-//     }).done(function() {
-//     alert('Your mail is sent!');
-//     }).fail(function(error) {
-//     alert('Oops… ' + JSON.stringify(error));
-//     })
-//     })
-//     })
-//     </script>
-
-
-
-// var myform = $("form#contact-form");
-// myform.submit(function(event){
-// 	event.preventDefault();
-
-//   // Change to your service ID, or keep using the default service
-//   var service_id = "gmail";
-//   var template_id = "template_2SvIv66k";
-
-//   myform.find("button").text("Sending...");
-//   emailjs.sendForm(service_id,template_id,myform[0])
-//   	.then(function(){ 
-//     	alert("Sent!");
-//        myform.find("button").text("Send");
-//     }, function(err) {
-//        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-//        myform.find("button").text("Send");
-//     });
-//   return false;
-// });
-
 var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
@@ -89,3 +34,13 @@ myform.submit(function(event){
     });
   return false;
 });
+
+
+function Circlle(el) {
+  $(el).circleProgress({fill: {color: '#3693ff'}})
+  .on('circle-animation-progress', function(event, progress, stepValue){
+    $(this).find('strong').text(String(stepValue.toFixed(2))+'%');
+  })
+}
+
+Circlle('round');
